cda3ef5e384ae8ad1ffee31c6999c128a2132971
Nigel Small <nigel.small@neotechnology.com>
2015-11-26 10:14:08 +0000

    Quick demo

---
+++ demo/basic.py
@@ -0,0 +1,9 @@
+from neo4j.v1 import GraphDatabase
+
+driver = GraphDatabase.driver("bolt://localhost")
+session = driver.session()
+
+for n, n_sq in session.run("UNWIND range(1, 5) as n RETURN n, n *n AS n_sq"):
+    print(n, n_sq)
+
+session.close()
